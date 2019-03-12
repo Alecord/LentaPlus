6bbcc7ce61c99db88ca4f402c4de328ac2758bc0
Alecord <besnna@gmail.com>
2019-03-12 14:29:39 +0300

    Html news parser added

--- LentaPlus/NewsFeed/StretchyViewController.swift
+++ LentaPlus/NewsFeed/StretchyViewController.swift
@@ -7,14 +7,16 @@ import SwiftSoup
 class StretchyViewController: UIViewController, UIScrollViewDelegate {
 
     private let scrollView = UIScrollView()
-    private let newsTitle = UILabel()
     private let timeTitle = UILabel()
     private let rubricTitle = UILabel()
     private let imageView = UIImageView()
+    private let imageViewCover = UIImageView()
     private let textContainer = UIView()
     private let separatorContainer = UIImageView(frame: CGRect(x: 0, y: 0, width: 5, height: 5))
     private let news: News
     var bodies: [Body]!
+    var styles: [String: [NSAttributedString.Key : Any]]!
+    
     
     init(news: News) {
         self.news = news
@@ -31,6 +33,54 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let mainFont = UIFont(name: "IBMPlexSerif", size: CGFloat(integerLiteral: 17)) ?? UIFont.systemFont(ofSize: CGFloat(integerLiteral: 17), weight: UIFont.Weight.regular)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.paragraphSpacing = mainFont.lineHeight * 1.25
+        //paragraphStyle.firstLineHeadIndent = 20
+        //paragraphStyle.lineSpacing = 17
+        /*
+        family: IBM Plex Serif Thin
+        name: IBMPlexSerif-ThinItalic
+        name: IBMPlexSerif-Thin
+        name: PingFangTC-Semibold
+        name: AppleSDGothicNeo-SemiBold
+        family: IBM Plex Serif Light
+        name: IBMPlexSerif-LightItalic
+        name: IBMPlexSerif-Light
+        family: IBM Plex Serif
+        name: IBMPlexSerif-Italic
+        name: IBMPlexSerif-BoldItalic
+        name: IBMPlexSerif-Bold
+        name: IBMPlexSerif
+         */
+        styles = [
+            "p" : [
+                NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                NSAttributedString.Key.foregroundColor: UIColor.black,
+                NSAttributedString.Key.font: mainFont
+            ],
+            "p_i" : [
+                NSAttributedString.Key.font: UIFont(name: "IBMPlexSerif-Italic", size: CGFloat(integerLiteral: 17)) ?? UIFont.systemFont(ofSize: CGFloat(integerLiteral: 17), weight: UIFont.Weight.regular)
+            ],
+            "p_b" : [
+                NSAttributedString.Key.font: UIFont(name: "IBMPlexSerif-Bold", size: CGFloat(integerLiteral: 17)) ?? UIFont.systemFont(ofSize: CGFloat(integerLiteral: 17), weight: UIFont.Weight.regular)
+            ],
+            "question" : [
+                NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                NSAttributedString.Key.foregroundColor: UIColor.black,
+                NSAttributedString.Key.font: UIFont(name: "IBMPlexSerif-BoldItalic", size: CGFloat(integerLiteral: 17)) ?? UIFont.systemFont(ofSize: CGFloat(integerLiteral: 17), weight: UIFont.Weight.regular)
+            ],
+            "link" : [
+                NSAttributedString.Key.foregroundColor: UIColor.secondColor,
+                NSAttributedString.Key.underlineColor: UIColor.secondColor,
+                NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
+            ],
+            "h1" : [
+                NSAttributedString.Key.foregroundColor: UIColor.black,
+                NSAttributedString.Key.font: UIFont(name: "PTRootUI-Bold", size: CGFloat(integerLiteral: 22)) ?? UIFont.systemFont(ofSize: CGFloat(integerLiteral: 22), weight: UIFont.Weight.bold)
+            ]
+        ]
+
         loadContent(url: news.link!)
         viewConfiguration()
     }
@@ -69,23 +119,57 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
         scrollView.delegate = self
         
         // Image configuration
+        var caption: String? = ""
+        var credits: String? = ""
         imageView.image = UIImage(named: "Header")
+        imageView.backgroundColor = .black
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         if (news.images?.count)! > 0 {
             for item in (news.images)! {
                 let image = item as! Image
                 let url = image.url ?? ""
+                caption = image.caption
+                credits = image.credits
                 if url != "" {
                     loadImage(url: url, place: imageView)
                     break
                 }
             }
         }
-        
+        imageViewCover.image = UIImage(named: "ImageCover")
+        imageViewCover.contentMode = .scaleAspectFill
+        imageViewCover.clipsToBounds = true
+        imageViewCover.alpha = 0.3
+
+        // Photo caption configuration
+        let caprionLabel = UILabel()
+        caprionLabel.numberOfLines = 0
+        let muttableText = NSMutableAttributedString()
+        if caption != nil {
+            let attributes1 =  [
+                NSAttributedString.Key.foregroundColor: UIColor.white,
+                NSAttributedString.Key.font: UIFont(name: "IBMPlexSerif-Bold", size: 13) ?? UIFont.systemFont(ofSize: 13, weight: UIFont.Weight.bold)
+            ]
+            let muttableCaption = NSMutableAttributedString(string: caption!, attributes: attributes1)
+            muttableText.append(muttableCaption)
+        }
+
+        if credits != nil {
+            let attributes2 =  [
+                NSAttributedString.Key.foregroundColor: UIColor.white,
+                NSAttributedString.Key.font: UIFont(name: "IBMPlexSerif", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.regular)
+            ]
+            let muttableCredits = NSMutableAttributedString(string: credits!, attributes: attributes2)
+            muttableText.append(NSMutableAttributedString(string: "\n"))
+            muttableText.append(muttableCredits)
+        }
+        caprionLabel.attributedText = muttableText
+
+
         // Time configuration
         timeTitle.textColor = UIColor.secondColor
-        timeTitle.font = UIFont(name: "PTRootUI-Regular", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.regular)
+        timeTitle.font = UIFont(name: "IBMPlexSerif", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.regular)
         timeTitle.numberOfLines = 0
         timeTitle.text = CoreDataManager.instance.GetTimeLocalosated(modified: Double(news.modified))
         
@@ -96,16 +180,10 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
 
         // Rubric configuration
         rubricTitle.textColor = .gray
-        rubricTitle.font = UIFont(name: "PTRootUI-Regular", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.regular)
+        rubricTitle.font = UIFont(name: "IBMPlexSerif", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.regular)
         rubricTitle.numberOfLines = 0
         let rubric: String = news.rubric ?? "Новое"
         rubricTitle.text = CoreDataManager.instance.GetRubricTitle(name: rubric)
-
-        // Title configuration
-        newsTitle.textColor = .black
-        newsTitle.font = UIFont(name: "PTRootUI-Bold", size: 22) ?? UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
-        newsTitle.numberOfLines = 0
-        newsTitle.text = news.title
 
         // Containers configuration
         let imageContainer = UIView()
@@ -123,7 +201,8 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
         scrollView.addSubview(textBacking)
         scrollView.addSubview(textContainer)
         scrollView.addSubview(imageView)
-        scrollView.addSubview(newsTitle)
+        scrollView.addSubview(imageViewCover)
+        scrollView.addSubview(caprionLabel)
         scrollView.addSubview(timeTitle)
         scrollView.addSubview(separatorContainer)
         scrollView.addSubview(rubricTitle)
@@ -148,9 +227,22 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
             make.bottom.equalTo(imageContainer.snp.bottom)
         }
         
+        imageViewCover.snp.makeConstraints {
+            make in
+            make.left.right.equalTo(imageContainer)
+            make.top.equalTo(view).priority(.high)
+            make.height.greaterThanOrEqualTo(imageContainer.snp.height).priority(.required)
+            make.bottom.equalTo(imageContainer.snp.bottom)
+        }
+
+        caprionLabel.snp.makeConstraints {
+            make in
+            make.left.equalTo(scrollView).offset(15)
+            make.bottom.equalTo(imageContainer.snp.bottom).offset(-15)
+        }
+
         textContainer.snp.makeConstraints {
             make in
-            
             make.top.equalTo(imageContainer.snp.bottom)
             make.left.right.equalTo(view)
             make.bottom.equalTo(scrollView)
@@ -176,13 +268,6 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
             make.top.equalTo(textContainer).offset(15)
         }
         
-        newsTitle.snp.makeConstraints {
-            make in
-            make.width.equalTo(view).offset(-30)
-            make.left.equalTo(textContainer).offset(15)
-            make.right.equalTo(textContainer).offset(15)
-            make.top.equalTo(timeTitle.snp.bottom).offset(10)
-        }
         
         textBacking.snp.makeConstraints {
             make in
@@ -196,62 +281,91 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func reloadContent(data: News) {
+        let contentText = UITextView()
+        contentText.textColor = .black
+        contentText.isUserInteractionEnabled = true
+        contentText.isEditable = false
+        contentText.isScrollEnabled = false
+        contentText.delaysContentTouches = false
+        textContainer.addSubview(contentText)
+        
+        let mutableText = NSMutableAttributedString(string: data.title!, attributes: styles["h1"])
+        mutableText.append(NSAttributedString(string: "\n\n"))
+        
+        contentText.snp.makeConstraints {
+            make in
+            make.width.equalTo(view).offset(-20)
+            make.left.equalTo(textContainer).offset(10)
+            make.right.equalTo(textContainer).offset(10)
+            make.top.equalTo(timeTitle.snp.bottom).offset(0)
+            make.bottom.equalTo(textContainer).offset(-10)
+        }
+
         let bCount: Int = data.bodies?.count ?? 0
         if bCount != 0 {
-            var i = 0
-            var lastTextLabel: UILabel?
             var bodies = data.bodies?.allObjects as! [Body]
             bodies = bodies.sorted(by: { $0.position < $1.position })
-            for body in bodies {
-                var ffont = UIFont(name: "PTRootUI-Regular", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.regular)
-                if body.type == "h1" {
-                    ffont = UIFont(name: "PTRootUI-Bold", size: 22) ?? UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
-                }
-                let contentText = UILabel()
-                contentText.textColor = .black
-                contentText.numberOfLines = 0
-                contentText.font = ffont
-                contentText.attributedText = parseContent(content: body.content ?? "")
-                contentText.isUserInteractionEnabled = true
+            
+            for (index, body) in bodies.enumerated() {
+                let type = body.type!
+                let params = styles[type] ?? [:]
+                var mutableBody = NSMutableAttributedString()
                 
-                textContainer.addSubview(contentText)
-                contentText.snp.makeConstraints {
-                    make in
-                    make.width.equalTo(view).offset(-30)
-                    make.left.equalTo(textContainer).offset(15)
-                    make.right.equalTo(textContainer).offset(15)
-                    if i == 0 {
-                        make.top.equalTo(newsTitle.snp.bottom).offset(15)
-                    } else if i == (data.bodies?.count)!-1 {
-                        make.bottom.equalTo(textContainer).offset(-15)
-                        make.top.equalTo(lastTextLabel!.snp.bottom).offset(20)
-                    } else {
-                        make.top.equalTo(lastTextLabel!.snp.bottom).offset(20)
-                    }
-                }
-                lastTextLabel = contentText
-                i += 1
+                // Parses
+                mutableBody = parseItalicBoldContent(content: body.content ?? "")
+                mutableBody = parseHrefContent(content: mutableBody.string, params: params)
+                
+                mutableText.append(mutableBody)
+                if index < bodies.count-1 {
+                    mutableText.append(NSAttributedString(string: "\n"))
             }
-            view.layoutIfNeeded()
-            view.updateConstraints()
+            }
+        } else {
+            let mutableRightcol = NSMutableAttributedString(string: data.rightcol!, attributes: styles["p"])
+            mutableText.append(mutableRightcol)
         }
+        contentText.linkTextAttributes = styles["link"]
+        contentText.attributedText = mutableText
+        
+        view.layoutIfNeeded()
+        view.updateConstraints()
+    }
+        
+    func parseItalicBoldContent(content: String) -> NSMutableAttributedString {
+        let attributedString = NSMutableAttributedString(string: content)
+        let elementsI: Elements = try! SwiftSoup.parse(content).select("i")
+        for element in elementsI.array() {
+            guard let text: String = try? element.html() else { break }
+            guard let outerHtml: String = try? element.outerHtml() else { break }
+            let foundRange: NSRange = (attributedString.string as NSString).range(of: outerHtml, options: .literal)
+            if foundRange.length > 0 {
+                attributedString.addAttributes(styles["p_i"]!, range: foundRange)
+                attributedString.mutableString.replaceOccurrences(of: outerHtml, with: text, options: [], range: foundRange)
+            }
+        }
+        let elementsB: Elements = try! SwiftSoup.parse(content).select("b")
+        for element in elementsB.array() {
+            guard let text: String = try? element.html() else { break }
+            guard let outerHtml: String = try? element.outerHtml() else { break }
+            let foundRange: NSRange = (attributedString.string as NSString).range(of: outerHtml, options: .literal)
+            if foundRange.length > 0 {
+                attributedString.addAttributes(styles["p_b"]!, range: foundRange)
+                attributedString.mutableString.replaceOccurrences(of: outerHtml, with: text, options: [], range: foundRange)
+            }
+        }
+        return attributedString
     }
     
-    func parseContent(content: String) -> NSAttributedString {
-        let attributedString = NSMutableAttributedString(string: content, attributes: nil)
+    func parseHrefContent(content: String, params: [NSAttributedString.Key : Any]) -> NSMutableAttributedString {
+        let attributedString = NSMutableAttributedString(string: content, attributes: params)
         guard let els: Elements = try? SwiftSoup.parse(content).select("a") else { return attributedString }
-        let linkAttributes: [NSAttributedString.Key : Any] = [
-            NSAttributedString.Key.foregroundColor: UIColor.secondColor,
-            NSAttributedString.Key.underlineColor: UIColor.secondColor,
-            NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
-        ]
         for element in els.array() {
-            //guard let hrefLink: String = try? element.attr("href") else { break }
+            guard let hrefLink: String = try? element.attr("href") else { break }
             guard let textLink: String = try? element.text() else { break }
             guard let outerHtml: String = try? element.outerHtml() else { break }
             let foundRange: NSRange = (attributedString.string as NSString).range(of: outerHtml, options: .literal)
-            attributedString.setAttributes(linkAttributes, range: foundRange)
             if foundRange.length > 0 {
+                attributedString.addAttribute(NSAttributedString.Key.link, value: hrefLink, range: foundRange)
                 attributedString.mutableString.replaceOccurrences(of: outerHtml, with: textLink, options: [], range: foundRange)
             }
         }
@@ -347,16 +461,19 @@ class StretchyViewController: UIViewController, UIScrollViewDelegate {
             return
         }
         if frame.minY <= 80 {
-            UIView.animate(withDuration: 0.4, animations: {
+            //UIView.animate(withDuration: 0.2, animations: {
                 statusBarView.backgroundColor = .darkGray
                 self.navigationController?.navigationBar.backgroundColor = .darkGray
-            })
+            //})
         } else {
-            UIView.animate(withDuration: 0.4, animations: {
+            //UIView.animate(withDuration: 0.2, animations: {
                 statusBarView.backgroundColor = .clear
                 self.navigationController?.navigationBar.backgroundColor = UIColor(red: 85, green: 85, blue: 85, alpha: 0)
-            })
+            //})
         }
+        // 305 -> 80  :  0.3 -> 1
+        let alp = 1.249 - 0.003 * frame.minY
+        self.imageViewCover.alpha = alp
     }
     
 }
